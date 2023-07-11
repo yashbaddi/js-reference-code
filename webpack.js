b2f78bddb62b169f9c943b66f0e4b91ee7cbab8e
Yash Baddi <yashbaddi29@gmail.com>
2023-07-11 12:42:57 +0530

    webpack

---
+++ webpack.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            configFile: path.resolve(__dirname, ".babelrc"),
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 3000,
+  },
+};
