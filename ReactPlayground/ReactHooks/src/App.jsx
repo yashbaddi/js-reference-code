d744fb1fb374d6988f612764087966670d0247eb
Yash Baddi <yashbaddi29@gmail.com>
2023-08-16 12:36:39 +0530

    Added a context

--- ReactPlayground/ReactHooks/src/App.jsx
+++ ReactPlayground/ReactHooks/src/App.jsx
@@ -1,26 +1,14 @@
 import { useState } from "react";
+import { myContext } from "./Store/context.js";
 
 import "./App.css";
+import { GreatGrandParent } from "./Components/greatGrandParent";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <myContext.Provider value={5}>
+      <GreatGrandParent />
+    </myContext.Provider>
   );
 }
 
---
+++ ReactPlayground/ReactHooks/src/Store/context.js
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+const defaultValue = 0;
+
+export const myContext = createContext(defaultValue);
