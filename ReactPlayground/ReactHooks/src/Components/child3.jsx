ddf276577e2b71c0d577dc768072896be837d056
Yash Baddi <yashbaddi29@gmail.com>
2023-08-16 12:57:39 +0530

    Static useContext

--- ReactPlayground/ReactHooks/src/Components/child3.jsx
+++ ReactPlayground/ReactHooks/src/Components/child3.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { myContext } from "../Store/context";
+
 export function Child3() {
+  const data = useContext(myContext);
   return (
     <div>
       <span>This is a child 3</span>
+      <span>The useContext Value is {data}</span>
     </div>
   );
 }
--- ReactPlayground/ReactHooks/src/Components/greatGrandParent.jsx
+++ ReactPlayground/ReactHooks/src/Components/greatGrandParent.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react";
 import { GrandParent } from "./grandParent";
+import { myContext } from "../Store/context";
 
 export function GreatGrandParent() {
+  const data = useContext(myContext);
   return (
     <div>
-      <span>This is a great grand Parent</span>
+      <span>
+        This is a great grand Parent
+        <br />
+      </span>
+      <span>The Value is {data}</span>
       <GrandParent />
     </div>
   );
